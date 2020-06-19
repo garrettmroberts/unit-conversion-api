fdf076fca2d3d0e0bf41c7504c1a0c7b1a78caf6
Garrett Roberts <garrettmroberts@gmail.com>
2020-06-19 15:45:36 -0500

    length conversion api finished

--- routes/length.js
+++ routes/length.js
@@ -7,6 +7,8 @@ router
     try {
       const splitReq = req.params.from.split(/([a-z].+)/, 2);
       const castTo = req.params.to;
+
+      // Valid input checking
       if (/[^\d.]+/.test(splitReq[0]) 
         || !/^mm$|^cm$|^m$|^km$|^in$|^ft$|^yd$|^mi$/.test(splitReq[1])
         || !/^mm$|^cm$|^m$|^km$|^in$|^ft$|^yd$|^mi$/.test(castTo)
@@ -14,10 +16,17 @@ router
         res.status(400).send({error: 'Invalid input.'})
       };
       
+      // Converts units
       const table = json[splitReq[1]];
       const multiply = table[castTo];
-      const converted = parseInt(splitReq[0])* multiply;
+      let converted = parseInt(splitReq[0])* multiply;
 
+      // Handles queries
+      if (req.query.precision) {
+        converted = parseFloat(converted.toFixed(parseInt(req.query.precision)));
+      };
+
+      // Send result
       res.json({
         "from": req.params.from,
         "to": req.params.to,
