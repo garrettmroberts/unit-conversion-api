a5b7a7f3eb6f3fb35771fbba76cd4ae9640a6c16
Garrett Roberts <garrettmroberts@gmail.com>
2020-06-24 13:13:52 -0500

    area routes added

--- routes/area.js
+++ routes/area.js
@@ -1,7 +1,7 @@
 const router = require('express').Router();
-const json = require('../assets/length.json');
+const json = require('../assets/area.json');
 
-// matches /length/:from/:to
+// matches /area/:from/:to
 router
   .route('/:from/:to')
   .get((req, res) => {
@@ -10,26 +10,26 @@ router
       const castTo = req.params.to;
 
       // Valid input checking
-      if (/[^\d.]+/.test(splitReq[0]) 
-        || !/^mm$|^cm$|^m$|^km$|^in$|^ft$|^yd$|^mi$/.test(splitReq[1])
-        || !/^mm$|^cm$|^m$|^km$|^in$|^ft$|^yd$|^mi$/.test(castTo)
+      if (/[^\d.]+/.test(splitReq[0])
+        || !/^mm$|^cm$|^m$|^km$|^in$|^ft$|^yd$/.test(splitReq[1])
+        || !/^mm$|^cm$|^m$|^km$|^in$|^ft$|^yd$/.test(castTo)
       ) {
-        res.status(400).send({error: 'Invalid input.'})
+        res.status(400).send({ error: 'Invalid input.' })
       };
-      
+
       // Converts units
       const table = json[splitReq[1]];
       const multiply = table[castTo];
-      let converted = parseFloat(splitReq[0])* multiply;
-      
+      let converted = parseFloat(splitReq[0]) * multiply;
+
       // Handles precision query
       if (req.query.precision) {
         converted = parseFloat(converted.toFixed(parseInt(req.query.precision)));
       };
 
       // Sets standard convertedStr and sentence
-      let convertedStr = converted.toString() + castTo;
-      let sentence = `${splitReq[0]}${splitReq[1]} is equal to ${converted}${castTo}.`;
+      let convertedStr = converted.toString() + castTo + "2";
+      let sentence = `${splitReq[0]}${splitReq[1]}2 is equal to ${converted}${castTo}2.`;
 
       // Handles subunits query
       if (req.query.subunits === 'true') {
@@ -50,9 +50,9 @@ router
           if (decimal) {
             let subunitMeasure = (json[castTo][subunit] * decimal).toFixed(2);
             convertedStr = `${converted.toFixed(0)}${castTo} and ${subunitMeasure}${subunit}`;
-            sentence = `${req.params.from} is equal to ${converted.toFixed(0)}${castTo} and ${subunitMeasure}${subunit}.`;
+            sentence = `${req.params.from}2 is equal to ${converted.toFixed(0)}${castTo}2 and ${subunitMeasure}${subunit}2.`;
           }
-          
+
         }
       };
 
@@ -65,10 +65,9 @@ router
         "convertedStr": convertedStr,
         "Sentence": sentence
       })
-
     } catch(error) {
       res.send(error);
-    }
-  })
+    };
+  });
 
-module.exports = router;
+  module.exports = router;
--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,12 @@
 const express = require('express');
 const path = require('path');
 const lengthRoutes = require('./length.js');
+const areaRoutes = require('./area.js');
 
 const router = express.Router();
 
 router.use('/length', lengthRoutes);
+router.use('/area', areaRoutes);
 
 router
   .get('/', (req, res) => {
